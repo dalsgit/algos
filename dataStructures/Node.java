763f499860a88035a464f36d56f223f9fee3b8c0
Daljeet Singh <daljeetsingh@15macbook.local>
2022-09-27 10:47:09 -0400

    1

---
+++ dataStructures/Node.java
@@ -0,0 +1,16 @@
+package dataStructures;
+
+public class Node {
+    public int data;
+    public Node next;
+    
+    public Node(int data) {
+        this.data = data;
+    }
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    
+}
