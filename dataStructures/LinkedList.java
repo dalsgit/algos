3053438cd17febb26be035f81ca48854b2f4c57e
daljeetsingh <daljeetsm@gmail.com>
2022-09-27 18:48:20 -0400

    1

--- dataStructures/LinkedList.java
+++ dataStructures/LinkedList.java
@@ -1,5 +1,17 @@
 package dataStructures;
 
 public class LinkedList {
-    
+    Node head;
+
+    public Node insert(int data, int locationIdx) {
+        Node newNode = new Node(data);
+        if locationIdx == -1 {
+            // add at the end
+            
+        }
+    }
+}
+
+public static void main(String args[]) {
+
 }
